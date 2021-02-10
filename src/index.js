9cc67e4e9ad7914911f6f3457790b08a98d9dd50
alla <a.barabasch@gmail.com>
2021-02-10 19:38:50 +0100

    initial project

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+let posts = [
+    {id: 1, message: 'Hi, how are you?', likesCount: 12},
+    {id: 2, message: 'It\'s my first post', likesCount: 11},
+    {id: 3, message: 'Blabla', likesCount: 11},
+    {id: 4, message: 'Dada', likesCount: 11}
+]
+
+let dialogs = [
+    {id: 1, name: 'Alla'},
+    {id: 2, name: 'Yulia'},
+    {id: 3, name: 'Nick'},
+    {id: 4, name: 'Thomas'},
+    {id: 5, name: 'Daniel'},
+    {id: 6, name: 'Marianne'}
+]
+
+let messages = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How is your it-kamasutra?'},
+    {id: 3, message: 'Yo'},
+    {id: 4, message: 'Yo'},
+    {id: 5, message: 'Yo'}
+]
+
+ReactDOM.render(<App posts={posts}
+                     dialogs={dialogs}
+                     messages={messages}
+                />,
+    document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
