c3f7b4f98dc501786249dc459d4dfaf32793f3d4
alla <a.barabasch@gmail.com>
2021-03-02 16:25:07 +0100

    added basic authentification

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 const reducers = combineReducers({
     profilePage: profileReducer, // each reducer is responsible for it's tree in the app
     dialogsPage: dialogsReducer,
     sidebarPage: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 })
 const store = createStore(reducers)
 
